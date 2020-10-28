df9651d689154cd3680e47e5eb92324534393082
samuai2603 <72535964+samuai2603@users.noreply.github.com>
2020-10-28 16:51:22 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php 
- $server = "localhost";
- $username = "postgres";
- $password = "123";
- $database = "test";
+ $server = "ec2-52-71-153-228.compute-1.amazonaws.com";
+ $username = "yshdhnftvtuers";
+ $password = "6642ff524d7329bed5281de27d236927715b0d3e9ed11c8e4b75032f31dead5a";
+ $database = "d7vmjhcd9u5d7i";
  $connString = "host = $server dbname = $database user = $username password = $password";
 ?>	 
